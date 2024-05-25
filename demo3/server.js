e84744b64e547ebd5f60f31de696557f941dc10e
XZ361 <2457307263@qq.com>
2024-05-26 05:15:41 +0800

    feat:add make登录

--- demo3/server.js
+++ demo3/server.js
@@ -1,16 +1,41 @@
+const http = require("http");
+const querystring = require("querystring");
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "123456xzX",
-  database: "demo3",
-});
+const app = http.createServer((req, res) => {
+  let postVal = "";
 
-connection.connect();
-connection.query("select * from user", (err, results, fields) => {
-  if (err) throw err;
-  console.log(results);
+  req.on("data", (chunk) => {
+    postVal += chunk;
+  });
+  req.on("end", () => {
+    let { userName, userPwd } = querystring.parse(postVal);
+    // console.log(userName, userPwd);
+
+    const connection = mysql.createConnection({
+      host: "localhost",
+      port: "3306",
+      user: "root",
+      password: "123456xzX",
+      database: "demo3",
+    });
+
+    connection.connect();
+    connection.query(
+      "select * from user where userName=? and userPwd=?",
+      [userName, userPwd],
+      (err, results, fields) => {
+        if (err) throw err;
+        if (results.length > 0) {
+          res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+          res.write("登陆成功");
+          res.end();
+        }
+      }
+    );
+    connection.end();
+  });
 });
-connection.end();
+app.listen(8080, () => {
+  console.log("服务启动成功");
+});
