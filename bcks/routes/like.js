90223d4bc61786d981f4ec0e8813acbfaa5c9f79
XZ361 <2457307263@qq.com>
2024-05-29 10:07:01 +0800

    feat:add select搜索功能

--- bcks/routes/like.js
+++ bcks/routes/like.js
@@ -3,49 +3,47 @@ var router = express.Router();
 var db = require("../sql.js");
 
 router.get("/", function (req, res, next) {
-  let inputVal = req.query.searchVal;
-  //   console.log(inputVal);
+  let like = req.query.like || req.query.selectVal;
+
+  console.log(like);
   let pageNum = req.query.page;
-  db.query(
-    `select * from banner where name like "%${inputVal}%"`,
-    (err, data) => {
-      if (err) {
-        throw err;
-      } else {
-        let pager = {};
-        let parent = [];
+  db.query(`select * from banner where name like "%${like}%"`, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      let pager = {};
+      let parent = [];
 
-        pager.pageSize = 5;
-        pager.pageCurrent = pageNum || 1;
-        // prev.length = pager.pageCurrent - 1;
-        // total.length = 10 - pager.pageCurrent;
+      pager.pageSize = 5;
+      pager.pageCurrent = pageNum || 1;
+      // prev.length = pager.pageCurrent - 1;
+      // total.length = 10 - pager.pageCurrent;
 
-        let dataList = data.slice(
-          (pager.pageCurrent - 1) * 5,
-          pager.pageCurrent * pager.pageSize
-        );
-        pager.maxNum = data.length;
-        pager.pages = Math.ceil(pager.maxNum / pager.pageSize);
+      let dataList = data.slice(
+        (pager.pageCurrent - 1) * 5,
+        pager.pageCurrent * pager.pageSize
+      );
+      pager.maxNum = data.length;
+      pager.pages = Math.ceil(pager.maxNum / pager.pageSize);
 
-        let prevP = pager.pageCurrent > 1 ? pager.pageCurrent : 1;
-        let nextP = pager.pages < 10 ? pager.pages : 10;
+      let prevP = pager.pageCurrent > 1 ? pager.pageCurrent : 1;
+      let nextP = pager.pages < 10 ? pager.pages : 10;
 
-        parent.total = {};
-        parent.prev = {};
+      parent.total = {};
+      parent.prev = {};
 
-        for (let i = 1; i < prevP; i++) {
-          parent.prev[i] = i;
-        }
-        for (let i = parseInt(pager.pageCurrent) + 1; i <= nextP; i++) {
-          parent.total[i] = i;
-        }
-        res.render("bannerList", {
-          bannerList: dataList,
-          pager,
-          parent,
-        });
+      for (let i = 1; i < prevP; i++) {
+        parent.prev[i] = i;
       }
+      for (let i = parseInt(pager.pageCurrent) + 1; i <= nextP; i++) {
+        parent.total[i] = i;
+      }
+      res.render("bannerList", {
+        bannerList: dataList,
+        pager,
+        parent,
+      });
     }
-  );
+  });
 });
 module.exports = router;
